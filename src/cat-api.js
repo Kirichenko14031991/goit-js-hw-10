c186816daea099bd0469f9d799e1a13c4594b6ed
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-07-15 14:28:55 +0300

    1

---
+++ src/cat-api.js
@@ -0,0 +1,23 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_vM9DBwLot7OqMUtH1zbD1vRRWEGo0rqBWQXlotSyvH1CEjMtYRHJRt55VWEZ1lXY';
+
+export function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
