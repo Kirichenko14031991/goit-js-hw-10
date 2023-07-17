a939c684c625fdc0624d4543c9118d6d7acfdea3
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-07-17 15:22:57 +0300

    perevirka

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,23 +1,27 @@
+import axios from 'axios';
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_vM9DBwLot7OqMUtH1zbD1vRRWEGo0rqBWQXlotSyvH1CEjMtYRHJRt55VWEZ1lXY';
 
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
 export function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds`).then(response => {
-    if (!response.ok) {
+  return axios.get(`${BASE_URL}/breeds`).then(response => {
+    if (response.status !== 200) {
+      console.log('There is an error while fetching a breed');
       throw new Error(response.statusText);
     }
-    return response.json();
+    return response.data;
   });
 }
 
 export function fetchCatByBreed(breedId) {
-  return fetch(
-    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
-  ).then(response => {
-    if (!response.ok) {
+  const breedUrl = `${BASE_URL}/images/search?breed_ids=${breedId}`;
+  return axios.get(breedUrl).then(response => {
+    if (response.status !== 200) {
+      console.log('There is an error while fetching a cat');
       throw new Error(response.statusText);
     }
-    return response.json();
+    return response.data;
   });
 }
---
+++ src/index.js
@@ -0,0 +1,94 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+let arrayOfCats = {};
+hideSelect();
+hideError();
+breedSelect.addEventListener('change', onSelect);
+
+function onSelect(event) {
+  showLoader();
+  fetchCatByBreed(event.target.value)
+    .then(data => {
+      let dataInformation = data[0].breeds[0];
+      let { name, description, temperament } = dataInformation;
+      console.log(dataInformation);
+      hideLoader();
+      hideError();
+      showCatInfo();
+      catInfo.innerHTML = `
+       <div class="cat-wrap">
+       <img class="cat-img" src="${data[0].url}" alt="${name}">
+       <div class="cat-info-div">
+         <h2 class="cat-title">${name}</h2>
+         <p class="cat-description">${description}</p>
+         <p class="cat-temperament"><span class="cat-temperament-span">Temperament:</span> ${temperament}</p>
+       </div>
+       </div>`;
+    })
+    .catch(error => {
+      showSelect();
+      hideLoader();
+      showError();
+      hideCatInfo();
+      console.log(error);
+    });
+}
+
+function changeValue(values) {
+  let markup = [];
+  values.map(element => {
+    markup.push(
+      `<option value="" disabled selected hidden>Select your cat</option><option value="${element.id}">${element.name}</option>`
+    );
+  });
+  breedSelect.innerHTML = markup.join(' ');
+}
+
+fetchBreeds()
+  .then(data => {
+    hideLoader();
+    showSelect();
+    arrayOfCats = data;
+    console.log(arrayOfCats);
+    changeValue(arrayOfCats);
+  })
+  .catch(error => {
+    showError();
+
+    console.log(error);
+  });
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function showError() {
+  error.style.display = 'block';
+}
+
+function showSelect() {
+  breedSelect.style.display = 'block';
+}
+
+function showCatInfo() {
+  catInfo.style.display = 'block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+function hideError() {
+  error.style.display = 'none';
+}
+
+function hideSelect() {
+  breedSelect.style.display = 'none';
+}
+
+function hideCatInfo() {
+  catInfo.style.display = 'none';
+}
